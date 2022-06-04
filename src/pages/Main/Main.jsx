f98e24d71b9937e72511db36cc8f156b624c9663
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2022-06-04 12:38:49 +0300

    filtering monsters functionality

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,18 +4,34 @@ import './Main.css'
 
 const Main = () => {
    const [data, setData] = useState([])
+   const [filteredData, setFilteredData] = useState([])
+
+   const searchFilter = (text) => {
+      console.log(text)
+      const textLower = text.toLowerCase()
+      
+      const filtered = data.filter(item => {
+         return item.name.toLowerCase().includes(textLower)
+      })
+
+      setFilteredData(filtered)
+   }
 
    useEffect(() => {
       axios.get('https://jsonplaceholder.typicode.com/users')
-         .then((response) => setData(response.data))
+         .then((response) => {
+            setData(response.data)
+            setFilteredData(response.data)
+         })
    }, [])
 
    console.log(data)
 
    return (
-      <div>
+      <div className='main-page-container'>
+         <input type="text" placeholder='Search monsters' onChange={(e) => searchFilter(e.target.value)} />
          {
-            data.map((user) => {
+            filteredData.map((user) => {
                return (
                   <div key={user.id}>
                      <h1>{user.name}</h1>
