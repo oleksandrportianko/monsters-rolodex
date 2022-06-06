247013f123d560932387808c4e3d46028171b730
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2022-06-06 12:32:27 +0300

    optimize a functional component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './Search.css'
 
 const Search = ({ setFilteredData, data }) => {
-
-   const searchFilter = (text) => {
-      const textLower = text.toLowerCase()
+   
+   const searchFilter = (event) => {
+      const textLower = event.target.value.toLowerCase()
       
       const filtered = data.filter(item => {
          return item.name.toLowerCase().includes(textLower)
@@ -14,7 +14,7 @@ const Search = ({ setFilteredData, data }) => {
    }
 
    return (
-      <input className='search-input' type="text" placeholder='Search monsters' onChange={(e) => searchFilter(e.target.value)} />
+      <input className='search-input' type="text" placeholder='Search monsters' onChange={searchFilter} />
    )
 }
 
